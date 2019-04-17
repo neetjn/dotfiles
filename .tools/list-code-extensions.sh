5906bcb7f1fbfe84906cf1671704cd52ddbb8b0e
John Nolette <jnolette@transparent.com>
2019-04-17 13:40:21 -0400

    Adding chmod for install script

--- .tools/list-code-extensions.sh
+++ .tools/list-code-extensions.sh
@@ -10,5 +10,6 @@ echo "#!/usr/bin/env bash\n" > $SCRIPT_LOCATION
 if [ $CODE_PATH ]; then
   echo "Visual Studio Code found, exporting list of extensions..."
   code --list-extensions | xargs -L 1 echo "code --install-extension" >> $SCRIPT_LOCATION
+  chmod +x $SCRIPT_LOCATION
   echo "Installation script dumped @ ${SCRIPT_LOCATION}"
 fi
